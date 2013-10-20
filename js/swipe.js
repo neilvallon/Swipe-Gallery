0734e87bfa82d268366a46b29d3f54f14754d09d
Neil Vallon <neilvallon@gmail.com>
2013-10-20 11:32:34 -0500

    Make request to flickr for group images and parse results into a standard json format.

--- js/swipe.js
+++ js/swipe.js
@@ -1,5 +1,24 @@
+flickrParser = function(json){
+	var imageList = []
+	$(json.items).each(function(i, image){
+		imageList.push({
+			'title':image.title,
+			'artist':image.author,
+			'url':image.link,
+			'image':image.media.m});
+	})
+	return imageList;
+}
+
 $(document).ready(function(){
 	$('.swipeGal').each(function(i, obj){
+		var feedid = $(obj).data('feed-id');		
+		var url = 'http://ycpi.api.flickr.com/services/feeds/groups_pool.gne?id='+feedid+'&jsoncallback=?';
 		
+		$.getJSON(url, {format: "json"})
+			.done(function(data){
+					parsedImages = flickrParser(data);
+					//console.log(parsedImages);
+				});
 	});
 });
