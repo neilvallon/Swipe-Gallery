8862adc7f9a8faa18f0a4ca3e5ba27c6b2819655
Neil Vallon <neilvallon@gmail.com>
2013-10-20 13:07:22 -0500

    Build distribution function to asign pixel values to each image.

--- js/swipe.js
+++ js/swipe.js
@@ -10,6 +10,30 @@ flickrParser = function(json){
 	return imageList;
 }
 
+distributeimages = function(imgNum, pxWidth){
+	builder = function(remainder, acumFun){
+		if(remainder == 0)
+			return acumFun;
+		var a = Math.ceil(imgNum / remainder)
+		var newFun = function(x){ return acumFun(x) + (x % a == 0) }
+		return builder(remainder - Math.floor(imgNum / a), newFun);
+	}
+	
+	base = function(x){ return Math.floor(pxWidth / imgNum) };
+	return builder(pxWidth % imgNum, base)
+}
+
+renderGallery = function(domObj, imgList){
+	var distribution = distributeimages(imgList.length, domObj.width());
+	console.log(distribution)
+	c=0;
+	for(i=1;i<=imgList.length;i++)
+		c += distribution(i);
+		console.log(c)
+}
+
+distributeimages(201, 454)
+
 $(document).ready(function(){
 	$('.swipeGal').each(function(i, obj){
 		var feedid = $(obj).data('feed-id');		
@@ -18,7 +42,7 @@ $(document).ready(function(){
 		$.getJSON(url, {format: "json"})
 			.done(function(data){
 					parsedImages = flickrParser(data);
-					//console.log(parsedImages);
+					renderGallery($(obj), parsedImages)
 				});
 	});
 });
