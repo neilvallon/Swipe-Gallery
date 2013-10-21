58194fcd9f75f4db8a246e441e478378768cf517
Neil Vallon <neilvallon@gmail.com>
2013-10-21 12:52:40 -0500

    Code cleanup and making JSLint happier.

--- js/swipe.js
+++ js/swipe.js
@@ -4,17 +4,19 @@
  * @param JSON json 
  * @return JSON
  */
-flickrParser = function(json){
-	var imageList = []
-	$(json.items).each(function(i, image){
+var flickrParser = function(json){
+	var imageList = [];
+	$(json.items).each(function(){
 		imageList.push({
-			'title':image.title,
-			'artist':image.author,
-			'url':image.link,
-			'image':image.media.m});
-	})
+			'title': this.title,
+			'artist': this.author,
+			'url': this.link,
+			'image': this.media.m
+		});
+	});
+	
 	return imageList;
-}
+};
 
 
 /**
@@ -25,28 +27,31 @@ flickrParser = function(json){
  * @param int pxWidth   Width of Current gallery
  * @return funtion(x)   Function takes an index 'x' and returns number of pixles assigned to that index
  */
-distributeimages = function(imgNum, pxWidth){
+var distributeimages = function(imgNum, pxWidth){
+	var builder, base;
 	builder = function(remainder, acumFun){
-		if(remainder == 0)
+		var a, newFun;
+		if(remainder === 0){
 			return acumFun;
+		}
 		
 		// Get the interval of images to skip before adding another pixle.
 		// Ceil prevents adding too many.
-		var a = Math.ceil(imgNum / remainder)
+		a = Math.ceil(imgNum / remainder);
 		
 		// Create a new anonymouse function that returns the sum pixles asigned at other intervals with
 		// 1 added if the index 'x' is within the current interval as well.
-		var newFun = function(x){ return acumFun(x) + (x % a == 0) }
+		newFun = function(x){ return acumFun(x) + (x % a === 0); };
 		
 		// Update remainder by removing pixles taken care of by this iteration
 		// while recusivly adding more layers untill remainder is 0.
 		return builder(remainder - Math.floor(imgNum / a), newFun);
-	}
+	};
 	
 	// Initalize base function as the number of pixles every image should recieve.
-	base = function(x){ return Math.floor(pxWidth / imgNum) };
-	return builder(pxWidth % imgNum, base)
-}
+	base = function(){ return Math.floor(pxWidth / imgNum); };
+	return builder(pxWidth % imgNum, base);
+};
 
 
 /**
@@ -57,10 +62,10 @@ distributeimages = function(imgNum, pxWidth){
  * @param JSON img      JSON containing information about the image to update with
  * @return void
  */
-changeImage = function(domObj, infoBar, img){
-	domObj.css('background-image', "URL('"+img.image.replace('_m', '')+"')");
-	infoBar.html("<h3>"+img.title+"</h3><h4>"+img.artist+"</h4>");
-}
+var changeImage = function(domObj, infoBar, img){
+	domObj.css('background-image', "URL('" + img.image.replace('_m', '') + "')");
+	infoBar.html("<h3>" + img.title + "</h3><h4>" + img.artist + "</h4>");
+};
 
 
 /**
@@ -71,31 +76,29 @@ changeImage = function(domObj, infoBar, img){
  * @param JSON[] imgList   Array of image information
  * @return void
  */
-renderGallery = function(domObj, imgList){
+var renderGallery = function(domObj, imgList){
+	var distribution, hoverContainer, infoContainer;
 	// Get function to map image indexes to pixle values
-	var distribution = distributeimages(imgList.length, domObj.width());
+	distribution = distributeimages(imgList.length, domObj.width());
 	
-	var hoverContainer = $("<div class='rContainer'></div>");
-	var infoContainer = $("<div class='info'></div>");
-	infoContainer.css('top', -(domObj.height()-15))
+	hoverContainer = $("<div class='rContainer'></div>");
+	infoContainer = $("<div class='info'></div>").css('top', 15 - domObj.height());
 	
-	domObj.append(hoverContainer);
-	domObj.append(infoContainer);
+	domObj.append([hoverContainer, infoContainer]);
 	
 	// Display first image
 	changeImage(domObj, infoContainer, imgList[0]);
 	
 	$(imgList).each(function(i, img){
 		// add rollover element for image with a width set to that calculated in distributeimages()
-		var hoverTemp = "<div class='rollover' style='width:"+distribution(i+1)+"px;'><\/div>"
-		var elm = $(hoverTemp);
-		hoverContainer.append(elm);
+		var elm = $("<div class='rollover'></div>")
+			.css('width', distribution(i+1))
+			.click(function(){ window.open(img.url, '_blank'); })
+			.mouseover(function(){ changeImage(domObj, infoContainer, img); });
 		
-		// Set up event listeners
-		elm.click(function(){ window.open(img.url, '_blank'); });
-		elm.mouseover(function(){ changeImage(domObj, infoContainer, img); });
+		hoverContainer.append(elm);
 	});
-}
+};
 
 
 /**
@@ -103,13 +106,14 @@ renderGallery = function(domObj, imgList){
  */
 $(document).ready(function(){
 	$('.swipeGal').each(function(i, obj){
-		var feedid = $(obj).data('feed-id');		
-		var url = 'http://ycpi.api.flickr.com/services/feeds/groups_pool.gne?id='+feedid+'&jsoncallback=?';
+		var feedid, url;
+		feedid = $(obj).data('feed-id');
+		url = 'http://ycpi.api.flickr.com/services/feeds/groups_pool.gne?id='+feedid+'&jsoncallback=?';
 		
 		$.getJSON(url, {format: "json"})
 			.done(function(data){
-					parsedImages = flickrParser(data);
-					renderGallery($(obj), parsedImages)
-				});
+				var parsedImages = flickrParser(data);
+				renderGallery($(obj), parsedImages);
+			});
 	});
 });
